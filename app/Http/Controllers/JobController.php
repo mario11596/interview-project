7dc6d8c11cb4a07185f7c17a50c3cbc207e417b9
Darijan Jelušić <darijan.jelusic@gmail.com>
2021-12-27 17:44:26 +0100

    Pojednostavljivanje crud funkcija i stvaranje ograničenja za tvrtke

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,29 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\Job;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class JobController extends Controller
 {
     public function index(Request $request) {
         if ($request->has('company_id')) {
-            $query = DB::table('jobs')->where('company_id', '=', $request->input('company_id'));
-        } else {
-            $query = DB::table('jobs');
+            return Job::where('company_id', $request->input('company_id'))->paginate($request->input('count', 10));
         }
-        return $query->paginate($request->input('count', 10));
+
+        return Job::paginate($request->input('count', 10));
     }
 
     public function create(Request $request) {
-        DB::table('jobs')->insert($request->all());
+        $id = Company::where('email_id', $request->user()->email)->value('id');
+        Job::create(['company_id' => $id] + $request->all());
     }
 
     public function delete(Request $request) {
-        /*if (DB::table('jobs')->where('id', '=', $request->input('job_id'))->get('company_id') != $request->input('company_id')) {
-            abort(403);
-        }*/
-        DB::table('jobs')->delete($request->input('job_id'));
+        if (Auth::user()->is_company) {
+            $company = Company::where('email_id', $request->user()->email)->value('id');
+            $job = Job::where('id', $request->input('job_id'))->value('company_id');
+            if ($company != $job) {
+                abort(403);
+            }
+        }
+        Job::where('id', $request->input('job_id'))->delete();
     }
 
 }
