3f0cb74246b17924a4f2746d48364d4d82f1ca27
mario_negovetic <mario.negovetic@estudent.hr>
2022-01-02 19:49:42 +0100

    dodane stranice za email i applications

---
+++ resources/views/email/emailTemplateCandidate.blade.php
@@ -0,0 +1,24 @@
+<form action="{{ route('candidate.email_send_applications', 2) }}" method="POST" >
+    @csrf
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                <strong>Naslov:</strong>
+                <input type="text" name="title" class="form-control" >
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Sadržaj:</strong>
+                <input class="form-control" name="content" type="text"/>
+            </div>
+        </div>
+
+        
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Pošalji</button>
+        </div>
+    </div>
+</form>
