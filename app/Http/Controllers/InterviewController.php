68abed7814789483487f15d7d96dcc816fda5e3c
Darijan Jelušić <darijan.jelusic@gmail.com>
2022-01-10 21:10:09 +0100

    Redirekcija pri spremanju intervjua

--- app/Http/Controllers/InterviewController.php
+++ app/Http/Controllers/InterviewController.php
@@ -39,6 +39,8 @@ class InterviewController extends Controller
     public function store(Request $request, $id) {
         $user_id = Candidate::where('email_id', Auth::user()->email)->value('candidate_id');
         Interview::create(['user_id' => $user_id, 'job_id' => $id] + $request->all());
+
+        redirect("/dashboard");
     }
 
     public function delete($id) {
